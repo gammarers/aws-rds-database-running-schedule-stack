de1635fb5912bdd733260f556ab5025c6bea2880
yicr <yicr@users.noreply.github.com>
2023-04-13 15:26:04 +0900

    chore: update projen setting

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { awscdk } from 'projen';
+import { awscdk, javascript } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
@@ -6,8 +6,22 @@ const project = new awscdk.AwsCdkConstructLibrary({
   defaultReleaseBranch: 'main',
   name: '@yicr/aws-rds-database-running-scheduler',
   description: 'AWS RDS Database Running Scheduler',
+  keywords: ['aws', 'cdk', 'aws-cdk', 'rds', 'scheduler', 'cost', 'saving'],
   projenrcTs: true,
   repositoryUrl: 'https://github.com/yicr/aws-rds-database-running-scheduler.git',
-  releaseToNpm: false,
+  releaseToNpm: true,
+  npmAccess: javascript.NpmAccess.PUBLIC,
+  minNodeVersion: '16.0.0',
+  workflowNodeVersion: '16.19.1',
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      schedule: javascript.UpgradeDependenciesSchedule.expressions(['0 19 * * *']),
+    },
+  },
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['yicr'],
+  },
 });
 project.synth();
