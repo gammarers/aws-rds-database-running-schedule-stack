44c4e1ed56f9d7a513a2108fb67b14bd332ce110
yicr <yicr@users.noreply.github.com>
2024-08-16 18:42:55 +0900

    chore(deps): cdk version to 2.120.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,7 +3,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
   authorOrganization: true,
-  cdkVersion: '2.80.0',
+  cdkVersion: '2.120.0',
   constructsVersion: '10.0.5',
   typescriptVersion: '5.4.x',
   jsiiVersion: '5.4.x',
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Stack, StackProps } from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as scheduler from 'aws-cdk-lib/aws-scheduler';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import { Construct } from 'constructs';
 
 export enum DatabaseType {
@@ -39,6 +41,283 @@ export class RdsDatabaseRunningScheduleStack extends Stack {
     const randomNameKey = crypto.createHash('shake256', { outputLength: 4 })
       .update(`${cdk.Names.uniqueId(scope)}-${cdk.Names.uniqueId(this)}`)
       .digest('hex');
+
+    // 👇 Succeed
+    const succeed = new sfn.Succeed(this, 'Succeed');
+
+    // 👇 Get DB Instance Resources (Filter by Tag)
+    const getDBInstanceResources = new tasks.CallAwsService(this, 'GetDBInstanceResources', {
+      iamResources: ['*'],
+      iamAction: 'tag:GetResources',
+      service: 'resourcegroupstaggingapi',
+      action: 'getResources',
+      parameters: {
+        ResourceTypeFilters: [
+          'rds:db',
+        ],
+        TagFilters: [
+          {
+            'Key.$': '$.Input.TagKey',
+            'Values.$': '$.Input.TagValues',
+          },
+        ],
+      },
+      resultPath: '$.Result',
+      resultSelector: {
+        'TargetResources.$': '$..ResourceTagMappingList[*].ResourceARN',
+      },
+    });
+
+    // 👇 Get DB Instance Resources (Filter by Tag)
+    const getDBClusterResources = new tasks.CallAwsService(this, 'GetDBClusterResources', {
+      iamResources: ['*'],
+      iamAction: 'tag:GetResources',
+      service: 'resourcegroupstaggingapi',
+      action: 'getResources',
+      parameters: {
+        ResourceTypeFilters: [
+          'rds:cluster',
+        ],
+        TagFilters: [
+          {
+            'Key.$': '$.Input.TagKey',
+            'Values.$': '$.Input.TagValues',
+          },
+        ],
+      },
+      resultPath: '$.Result',
+      resultSelector: {
+        'TargetResources.$': '$..ResourceTagMappingList[*].ResourceARN',
+      },
+    });
+
+    // 👇 Describe DB Instance Task
+    const describeDBInstancesTask = new tasks.CallAwsService(this, 'DescribeDBInstances', {
+      iamResources: [`arn:aws:rds:*:${account}:db:*`],
+      service: 'rds',
+      action: 'describeDBInstances',
+      parameters: {},
+      resultPath: '$.Result',
+      outputPath: '$.Result.DbInstances[?(@.DbInstanceIdentifier == $.TargetDBInstanceIdentifier)]',
+    });
+
+    // 👇 Stop DB Instance Task
+    const stopDBInstanceTask = new tasks.CallAwsService(this, 'StopDBInstance', {
+      iamResources: [`arn:aws:rds:*:${account}:db:*`],
+      service: 'rds',
+      action: 'stopDBInstance',
+      parameters: {
+        'DbInstanceIdentifier.$': '$[0].DbInstanceIdentifier',
+      },
+    }).next(succeed);
+
+    // 👇 Start DB Instance Task
+    const startDBInstanceTask = new tasks.CallAwsService(this, 'StartDBInstance', {
+      iamResources: [`arn:aws:rds:*:${account}:db:*`],
+      service: 'rds',
+      action: 'stopDBInstance',
+      parameters: {
+        'DbInstanceIdentifier.$': '$[0].DbInstanceIdentifier',
+      },
+    }).next(succeed);
+
+    // 👇 Describe DB Cluster Task
+    const describeDBClustersTask = new tasks.CallAwsService(this, 'DescribeDBClusters', {
+      iamResources: [`arn:aws:rds:*:${account}:cluster:*`],
+      service: 'rds',
+      action: 'describeDBClusters',
+      parameters: {},
+      resultPath: '$.Result',
+      outputPath: '$.Result.DbClusters[?(@.DbClusterIdentifier == $.detail.SourceIdentifier)]',
+    });
+
+    // 👇 Stop DB Cluster Task
+    const stopDBClusterTask = new tasks.CallAwsService(this, 'StopDBCluster', {
+      iamResources: [`arn:aws:rds:*:${account}:cluster:*`],
+      service: 'rds',
+      action: 'stopDBCluster',
+      parameters: {
+        'DbClusterIdentifier.$': '$[0].DbClusterIdentifier',
+      },
+    }).next(succeed);
+
+    // 👇 Stop DB Cluster Task
+    const startDBClusterTask = new tasks.CallAwsService(this, 'StartDBCluster', {
+      iamResources: [`arn:aws:rds:*:${account}:cluster:*`],
+      service: 'rds',
+      action: 'stopDBCluster',
+      parameters: {
+        'DbClusterIdentifier.$': '$[0].DbClusterIdentifier',
+      },
+    }).next(succeed);
+
+    const startDBInstanceStatusChangeWait = new sfn.Wait(this, 'StartDBInstanceStatusChangeWait', {
+      time: sfn.WaitTime.duration(cdk.Duration.minutes(1)),
+    });
+
+    const stopDBInstanceStatusChangeWait = new sfn.Wait(this, 'StopDBInstanceStatusChangeWait', {
+      time: sfn.WaitTime.duration(cdk.Duration.minutes(1)),
+    });
+
+    const startDBClusterStatusChangeWait = new sfn.Wait(this, 'StartDBClusterStatusChangeWait', {
+      time: sfn.WaitTime.duration(cdk.Duration.minutes(1)),
+    });
+
+    const stopDBClusterStatusChangeWait = new sfn.Wait(this, 'StopDBClusterStatusChangeWait', {
+      time: sfn.WaitTime.duration(cdk.Duration.minutes(1)),
+    });
+
+    // Inputとして、モードを取得し、開始か終了かを取得
+    // Inputとして、タイプを取得し、インスタンスかクラスターかを取得
+    // インスタンスの場合
+    //  リソースタグを使って、対象となるリソースを取得する
+    //  リソースをそれぞれdescribeし起動中か停止中かを取得
+    //  ・モードが開始でリソースが停止中の場合に、開始処理を実施する。それ以外の場合は、Skipして終了する（メールを送る？）。
+    //  ・モードが停止でリソースが起動中の場合に、停止処理を実施する。それ以外の場合は、Skipして終了する（メールを送る？）。
+    //    ・開始処理をしてWaiting=60sする
+    //        開始モードの場合、開始していたら終了する。開始していなかったらWaitingを繰り返す。
+    //        停止モードの場合、停止していたら終了する。開始していなかったらWaitingを繰り返す。
+    // クラスターの場合
+    //  リソースタグを使って、対象となるリソースを取得する
+    //  リソースをそれぞれdescribeし起動中か停止中かを取得
+    //  ・モードが開始でリソースが停止中の場合に、開始処理を実施する。それ以外の場合は、Skipして終了する（メールを送る？）。
+    //  ・モードが停止でリソースが起動中の場合に、停止処理を実施する。それ以外の場合は、Skipして終了する（メールを送る？）。
+    const modeChoice = new sfn.Choice(this, 'ModeChoice')
+      .when(
+        sfn.Condition.and(sfn.Condition.stringEquals('$.Input.Mode', 'Start'), sfn.Condition.stringEquals('$.Input.Type', 'Instance')),
+        getDBInstanceResources.next(
+          new sfn.Map(this, 'StartInstancesMap', {
+            itemsPath: sfn.JsonPath.stringAt('$.Result.TargetResources'),
+            maxConcurrency: 10,
+          }).iterator(
+            new sfn.Pass(this, 'GetStartDBInstanceIdentifier', {
+              parameters: {
+                'TargetDBInstanceIdentifier.$': "States.ArrayGetItem(States.StringSplit($, ':'), 6)",
+              },
+            }).next(
+              describeDBInstancesTask.next(
+                new sfn.Choice(this, 'StartDBInstanceStatusChoice').when(
+                  sfn.Condition.stringEquals('$[0].DbInstanceStatus', 'stopped'),
+                  startDBInstanceTask.next(startDBInstanceStatusChangeWait.next(describeDBInstancesTask.next(
+                    new sfn.Choice(this, 'Choice')
+                      .when(
+                        sfn.Condition.stringEquals('$[0].DbInstanceStatus', 'starting'),
+                        startDBInstanceStatusChangeWait,
+                      )
+                      .when(
+                        sfn.Condition.stringEquals('$[0].DbInstanceStatus', 'available'),
+                        succeed,
+                      )
+                      .otherwise(new sfn.Fail(this, 'StartDbInstanceStatusFail', {
+                        cause: 'start db instance status fail.',
+                      })),
+                  ))),
+                ),
+              ),
+            ))),
+      )
+      .when(
+        sfn.Condition.and(sfn.Condition.stringEquals('$.Input.Mode', 'Stop'), sfn.Condition.stringEquals('$.Input.Type', 'Instance')),
+        getDBInstanceResources.next(
+          new sfn.Map(this, 'StopInstancesMap', {
+            itemsPath: sfn.JsonPath.stringAt('$.Result.TargetResources'),
+            maxConcurrency: 10,
+          }).iterator(
+            new sfn.Pass(this, 'GetStopDBInstanceIdentifier', {
+              parameters: {
+                'TargetDBInstanceIdentifier.$': "States.ArrayGetItem(States.StringSplit($, ':'), 6)",
+              },
+            }).next(
+              describeDBInstancesTask.next(
+                new sfn.Choice(this, 'StopDBInstanceStatusChoice').when(
+                  sfn.Condition.stringEquals('$[0].DbInstanceStatus', 'available'),
+                  stopDBInstanceTask.next(stopDBInstanceStatusChangeWait.next(describeDBInstancesTask.next(
+                    new sfn.Choice(this, 'Choice')
+                      .when(
+                        sfn.Condition.stringEquals('$[0].DbInstanceStatus', 'stopping'),
+                        stopDBInstanceStatusChangeWait,
+                      )
+                      .when(
+                        sfn.Condition.stringEquals('$[0].DbInstanceStatus', 'stopped'),
+                        succeed,
+                      )
+                      .otherwise(new sfn.Fail(this, 'StartDbInstanceStatusFail', {
+                        cause: 'stop db instance status fail.',
+                      })),
+                  ))),
+                ),
+              ),
+            ))),
+      )
+      .when(
+        sfn.Condition.and(sfn.Condition.stringEquals('$.Input.Mode', 'Start'), sfn.Condition.stringEquals('$.Input.Type', 'Cluster')),
+        getDBClusterResources.next(
+          new sfn.Map(this, 'StartClustersMap', {
+            itemsPath: sfn.JsonPath.stringAt('$.Result.TargetResources'),
+            maxConcurrency: 10,
+          }).iterator(
+            new sfn.Pass(this, 'GetStartDBClusterIdentifier', {
+              parameters: {
+                'TargetDBClusterIdentifier.$': "States.ArrayGetItem(States.StringSplit($, ':'), 6)",
+              },
+            }).next(
+              describeDBClustersTask.next(
+                new sfn.Choice(this, 'StartDBClusterStatusChoice').when(
+                  sfn.Condition.stringEquals('$[0].Status', 'stopped'),
+                  startDBClusterTask.next(startDBClusterStatusChangeWait.next(describeDBClustersTask.next(
+                    new sfn.Choice(this, 'Choice')
+                      .when(
+                        sfn.Condition.stringEquals('$[0].Status', 'starting'),
+                        startDBClusterStatusChangeWait,
+                      )
+                      .when(
+                        sfn.Condition.stringEquals('$[0].Status', 'available'),
+                        succeed,
+                      )
+                      .otherwise(new sfn.Fail(this, 'StartDBClusterStatusFail', {
+                        cause: 'start db clusater status fail.',
+                      })),
+                  ))),
+                ),
+              ),
+            ))),
+      )
+      .when(
+        sfn.Condition.and(sfn.Condition.stringEquals('$.Input.Mode', 'Stop'), sfn.Condition.stringEquals('$.Input.Type', 'Cluster')),
+        getDBClusterResources.next(
+          new sfn.Map(this, 'StopClusterMap', {
+            itemsPath: sfn.JsonPath.stringAt('$.Result.TargetResources'),
+            maxConcurrency: 10,
+          }).iterator(
+            new sfn.Pass(this, 'GetStopDBClusterIdentifier', {
+              parameters: {
+                'TargetDBClusterIdentifier.$': "States.ArrayGetItem(States.StringSplit($, ':'), 6)",
+              },
+            }).next(
+              describeDBClustersTask.next(
+                new sfn.Choice(this, 'StopDBClusterStatusChoice').when(
+                  sfn.Condition.stringEquals('$[0].Status', 'available'),
+                  stopDBClusterTask.next(stopDBClusterStatusChangeWait.next(describeDBClustersTask.next(
+                    new sfn.Choice(this, 'Choice')
+                      .when(
+                        sfn.Condition.stringEquals('$[0].Status', 'stopping'),
+                        stopDBClusterStatusChangeWait,
+                      )
+                      .when(
+                        sfn.Condition.stringEquals('$[0].Status', 'stopped'),
+                        succeed,
+                      )
+                      .otherwise(new sfn.Fail(this, 'StopDBClusterStatusFail', {
+                        cause: 'stop db instance status fail.',
+                      })),
+                  ))),
+                ),
+              ),
+            ))),
+      )
+      .otherwise(new sfn.Fail(this, 'ModeChoiceFail', {
+        cause: 'input argment Mode is unmatch.',
+      }));
 
     // 👇EventBridge Scheduler IAM Role
     const schedulerExecutionRole = new iam.Role(this, 'SchedulerExecutionRole', {
