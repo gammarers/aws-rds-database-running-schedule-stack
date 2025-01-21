4c4f835e0a8155b34ac9ef96780ce23eeef8cf9d
yicr <yicr@users.noreply.github.com>
2025-01-21 18:54:51 +0900

    fix: package version to ^0.2.4

--- .projenrc.ts
+++ .projenrc.ts
@@ -14,11 +14,11 @@ const project = new awscdk.AwsCdkConstructLibrary({
   repositoryUrl: 'https://github.com/gammarers/aws-rds-database-running-schedule-stack.git',
   deps: [
     '@gammarers/aws-resource-naming@^0.10.1',
-    '@gammarers/aws-sns-slack-message-lambda-subscription@^0.2.1',
+    '@gammarers/aws-sns-slack-message-lambda-subscription@^0.2.4',
   ],
   peerDeps: [
     '@gammarers/aws-resource-naming@^0.10.1',
-    '@gammarers/aws-sns-slack-message-lambda-subscription@^0.2.1',
+    '@gammarers/aws-sns-slack-message-lambda-subscription@^0.2.4',
   ],
   releaseToNpm: true,
   npmAccess: javascript.NpmAccess.PUBLIC,
